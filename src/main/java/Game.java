3c98d8d522f35565d983b97d925932a5fba1f2f0
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2025-03-24 20:40:23 +0200

    Added players list

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,6 +1,19 @@
+/**
+ * Description: Implement a two-player tic-tac-toe
+    game. Display the game board and prompt each
+    player to enter their moves. Check for a winning
+    condition or a draw after each move, and display
+    the result accordingly. Allow the players to play
+    multiple rounds if desired.
+ */
+
+
+
 public class Game {
     public static void main(String[] args){
-        System.out.println("hjI");
+      String[] players = {"Player_1","Player_2"};
+      
+
 
     }
 }
