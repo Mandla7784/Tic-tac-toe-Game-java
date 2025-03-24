9253afece6edf865b90e151385b30054bcc5dfdc
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2025-03-24 20:46:42 +0200

    working on game board

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -12,7 +12,15 @@
 public class Game {
     public static void main(String[] args){
       String[] players = {"Player_1","Player_2"};
-      
+      String[] gameBoard = {"_","|","_","|","_","|","_","|","_","|","_","|"};
+
+        
+      for(int i = 0 ; i < gameBoard.length ; i ++){
+        for(int j  = 0 ; j < gameBoard.length; j ++){
+                System.out.println(gameBoard[i]);
+        }
+        System.out.println();
+      }
 
 
     }
